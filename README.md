11dedc6de6e69630cd0e3a843a0dcf0b13fd1e10
Vincent Russo <vincent.russo@isara.com>
2018-04-13 15:28:51 -0400

    Updating README and setting up initial scaffolding

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # scirate_extractor
+
 Python wrapper for extracting information from Scirate :microscope:
+
+This project is undergoing development.
+
+|Build Status| |Coverage Status| |Documenation Status| |Downloads| |Latest Version| |Supported Python Versions| |License|
+
+.. image:: https://imgur.com/a/4Smh3  
+   :width: 100
+   :height: 100
+
+This package provides a Python interface for the `Scirate website <https://scirate.com>`__.
+
+Dependencies
+------------
+
+This package depends on the following packages:
+
+- xmltodict
+- requests
+
+They can be installed using ``pip``.
+
+::
+
+    sudo pip install -r requirements.txt
+
+If you want to contribute to this package, you will need the ``nose`` package as well.
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name="scirate_extractor",
+    description="Python wrapper for extracting content from Scirate.",
+    long_description=open("README.md").read(),
+    url="https://github.com/vprusso/scirate_extractor",
+
+    author="Vincent Russo",
+    author_email="vincentrusso1@gmail.com",
+
+    packages=["scirate_extractor"],
+    version="0.0.1",
+    install_requires=['nose', 'xmltodict', 'requests'],
+
+    license="MIT",
+
+    keywords="scirate API",
+
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+    ]
+)
