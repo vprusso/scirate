4c7f1c3c735da2d375482ae430c89474d632c2ff
vprusso <vincentrusso1@gmail.com>
2018-04-15 16:21:29 -0400

    Adding docs and updating various things

---
+++ docs/index.rst
@@ -0,0 +1,20 @@
+.. scirate documentation master file, created by
+   sphinx-quickstart on Sun Apr 15 15:04:59 2018.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to scirate's documentation!
+===================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="scirate",
     description="Python wrapper for extracting content from Scirate.",
-    long_description=open("README.md").read(),
+    long_description=open("README.rst").read(),
     url="https://github.com/vprusso/scirate",
 
     author="Vincent Russo",
@@ -15,7 +15,7 @@ setup(
 
     license="MIT",
 
-    keywords="scirate API",
+    keywords=["scirate", "scirate API", "scirate python"],
 
     classifiers=[
         "Development Status :: 4 - Beta",
@@ -24,5 +24,9 @@ setup(
         "Natural Language :: English",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.5",
-    ]
+    ],
+
+    project_urls={
+        "Homepage": "http://vprusso.github.io/",
+    }
 )
