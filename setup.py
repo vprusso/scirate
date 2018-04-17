37fb0bf4cc83b1a6e1471a56f99472fcadb8c148
vprusso <vincentrusso1@gmail.com>
2018-04-17 18:47:03 -0400

    Adding to setup

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author_email="vincentrusso1@gmail.com",
 
     packages=["scirate"],
-    version="0.0.2",
+    version="0.0.3",
     install_requires=['nose', 'bs4', 'lxml', 'requests'],
 
     license="MIT",
